baebe9d420c49185f309eb9ead41768351c7f7ac
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-01-29 17:48:47 +0530

    second commit

--- src/Components/sidemenu.tsx
+++ src/Components/sidemenu.tsx
@@ -37,20 +37,20 @@ const Sidemenu = (props:any) => {
     <div>
     <div className={state} style={{marginTop:`${props.height}`}}>
     <div className="popup-content">
-      <h1 style={{color:"black"}} className="label">Fill Form to Download Brouchure</h1>
-      
+      <h1 style={{color:"black", textDecoration:"underline", paddingBottom:"7px"}} className="label">Fill Form to Download Brouchure</h1>
+    
       <p><form action="/popup.php" name="brouchereform"  method="POST" id="frmdb">
              <div id="sucess1"></div>               
                 <div className="inputGroup2">
-                  <input className="input" placeholder="Your emailId" type="email" name="email" id="email1"/><br/>
+                  <input className="input" style={{height:"30px", fontSize:"15px", padding:"10px 6px"}} placeholder="Your emailId" type="email" name="email" id="email1"/><br/>
                   <label >*Email</label>
                 </div>
                 <div className="inputGroup2">
-                  <input className="input" type="number" placeholder="Your Phone Number" required name="mobile" id="mobile1"/><br/>
+                  <input className="input" style={{height:"30px", fontSize:"15px", padding:"10px 6px"}} type="number" placeholder="Your Phone Number" required name="mobile" id="mobile1"/><br/>
                   <label>*Mobile</label>
                 </div>             
                 <div className="btn-group" >
-                  <button  className="tabelbuttonabcde" style={{backgroundColor:"green"}} type="submit" name="submit">Submit</button>
+                  <button  className="tabelbuttonabcde" style={{backgroundColor:"green", height:"32px", padding:"0px 10px 3px", marginTop:"10px"}} type="submit" name="submit">Submit</button>
                 </div>
               </form> </p>
            </div>  
@@ -58,21 +58,22 @@ const Sidemenu = (props:any) => {
   </div>
 
   <div className={costone}>
-    <div className="popup-content" style={{height:"180px"}}>
-    <h1 style={{color:"black"}} className="label">Fill Form to Download cost sheet</h1>
-      
+    <div className="popup-content" style={{height:"186px"}}>
+    <h1 style={{color:"black", textDecoration:"underline", paddingBottom:"7px"}} className="label">Fill Form to Download cost sheet</h1>
+    
      <form action="/popup.php"  method="POST" className="callRequest" id="frmps">
              <div id="sucess2"></div>      
                 <div className="inputGroup2">
-                  <input className="input" type="email" placeholder="Your Emailid" name="email" id="email2"/><br/>
+                  <input className="input" style={{height:"30px", fontSize:"15px", padding:"10px  6px"}} type="email" placeholder="Your Emailid" name="email" id="email2"/><br/>
                   <label>*Email</label>
                 </div>
                 <div className="inputGroup2">
-                  <input className="input" type="number" required placeholder="Your Number" name="mobile" id="mobile2"/><br/>
+                  <input className="input" style={{height:"30px", fontSize:"15px", padding:"10px 6px"}}  type="number" required placeholder="Your Number" name="mobile" id="mobile2"/><br/>
                   <label>*Mobile</label>
-                </div>             
+                </div>          
+                
                 <div className="btn-group">
-                  <button name="submit" className="tabelbuttonabcde" style={{backgroundColor:"green"}}type="submit">submit</button>
+                  <button name="submit" className="tabelbuttonabcde" style={{backgroundColor:"green", height:"30px", padding:"0px 10px 3px", marginTop:"10px"}} type="submit">submit</button>
                 </div>
               </form> 
     </div>  
