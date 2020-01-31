0d24e44550663fcae7c77b9655e342398fefb366
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-01-31 10:22:45 +0530

    second build

--- src/Components/Finalscreen.tsx
+++ src/Components/Finalscreen.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Title from './Title';
-import {Link} from 'react-router-dom';
+import {Link, BrowserRouter} from 'react-router-dom';
  
 const Finalscreen = () => {
     return (
+    
         <div style={{textAlign:"center"}}>
+            <BrowserRouter>
             <Title title="Thank you for submitting your details"/>
             <br/>
             <a href="https://drive.google.com/open?id=1Qf1ftT3t1EHLN9cdxjLiaIFAsMCS5yl4">Download Brouchere</a>
             <br/>
             <a href="https://drive.google.com/open?id=10MFgV_joBKJsfkIDIeasShVqMbWbxWyC">Download Costsheet</a>
+            <br/>
             <div>
-               <Link to="/"><button className="tabelbutton">Home</button></Link> 
+               <Link to="/"><button className="tabelbutton">Home</button></Link>
+              
             </div>
+            </BrowserRouter> 
         </div>
     );
 };
