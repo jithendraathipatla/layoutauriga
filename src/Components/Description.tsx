b532674150343668aa873ef570e5b670eafc5ec3
jithendraathipatla <athipatala.jithendra@gmail.com>
2020-02-29 13:40:41 +0530

    second commit

--- src/Components/Description.tsx
+++ src/Components/Description.tsx
@@ -8,23 +8,38 @@ const Description = () => {
             <div style={{textAlign:"center"}}>
              <TitleForDescription title="About Concorde Auriga"/>
             </div>
-       
-            <a href="http://concordeauriga.org.in/">Concorde Auriga Review </a> has the prepared to possess the property in Medahalli Bangalore. 
-                   Concorde has delightful and prepared to move-in condos, the property is spread crosswise over 5.2 Acres of land situated at Old Madras Road Bombay Trunk Rd, Medahalli , <a href="http://www.prestigewaterfordwhitefield.co/">Whitefield </a> East Bangalore. The quantity of units under this property is 500 with a rise3 blocks and G + 29 Floors, And Amenities at Concorde Auriga include a fully equipped clubhouse, landscaped gardens, gymnasium, swimming pool,
-                    recreation rooms, outdoor sports courts, children’s play area, party hall, and meticulously planned with utmost importance to state-of-the-art 24/7 securities.
-                     Concorde Auriga is situated in a large open area from 3 sides; Auriga is giving a great perspective on Lake. The architect attempted to their best to give progressively
-                      open space, daylight, and great air into the Residents house <br/> <br/> <a href="http://concordeauriga.org.in/">Concorde Auriga</a> is an independent private task that
-                       has been obviously built at a helpful area offering you the freedom to get to different open scenes, for example, schools, clinics, cafés, shopping centers, tech parks,
-                        and IT workplaces well-inside a short separation. Living here involves an all encompassing way of life that is rendered through first class enhancements, a steady all
-                         encompassing perspective on the lake and premium offices inside the premises. What separates Auriga is the opportunity it offers you to modify it according to your
-                          loving during its last leg of finishing. <br/> <br/> Concorde Auriga accepts that any living space is practical in the event that it is upheld by the simple system 
-                          of different foundations of fundamental pleasantries like emergency clinics, schools, travel focuses, and so on. Hence we reviewed the region and present to you the
-                           genuine ground-level report. The undertaking is arranged from Mg Road a ways off of 14.9 Km. Closest Railway Station is simply 5.9 Km away. Good ways from Airport
-                            is 31.5 Km as it were. Indeed, even Nearest Metro Station is simply 10.8 Km away while Nearest Ring Road is 13.2 km away. And Nearest Railway station is 
-                            <a href="http://concordeauriga.org.in/">KR Puram</a> <br/> <br/> Indeed, even the district has a decent assortment of human services offices in its region.
-                             Columbia Asia Hospital, Chinmaya Mission Hospital, Specialist Hospital, Svastha Hospital, Chaya Super Speciality Hospital, VIMS Hospital, Marathahali,
-                              Aayug Multi Speciality Hospital, Janapriya Hospital, MGA Hospital, Hope Hospital, Medihope Super Specialty Hospital Pvt Ltd, Patil Hospital, 
-                    Sri Ram emergency clinic are a portion of the couple of set up names in restorative consideration division here.                    </div>
+           
+
+
+  <a href="http://concordeauriga.org.in/">Concorde Auriga Review </a> is the new launch Property in Medahalli, near KR Puram Old Madras Road, Bangalore. The Property is spread across 5.2 Acer Land at Old Madras Road, Medahalli. This property has more than 500 units with 3 Towers and has G + 29 Floors. Amenities inside this Beautifully Crafted Project include Clubhouse, Landscaped Garden, Gymnasium, Swimming Pool, 24/7 Power Backup, Outdoor Sports Club, Children’s Play Area, Convention Hall, and with most of the vacant area dedicated to the Fresh and Natural Air. Beside this project you would have a beautiful lake, where you would be able to enjoy nature at its best in a vast city like Bangalore. The builders of this project are from concorde Group, they have designed this project in such a way that the sunlight will be available in all parts of the day and will have a good and quality air access to all the Residents of this beautifully planned Project.
+
+ <br/>
+ <br/>
+
+  <a href="http://concordeauriga.org.in/">Concorde Auriga</a> is coming_up in an area where the city has access to Bangalore's best Schools, Parks, Clinic’s, Tech parks’s, Shopping Centers and IT work-spaces. On any given day one could reach these spaces within a short span of Time. Auriga has beautifully designed homes, where you would be able to see the lake view through the glass view, this view gives a lot of happiness to all the nature Lovers. What separates you from enjoying the bliss inside the   <a href="http://concordeauriga.org.in/">Concorde Auriga</a> is its possession. It is in its last leg before opening its gates to the fortunate customers.
+  
+  
+ <br/>
+ <br/>
+
+Talking about the access to the famous parts of Bangalore, this project has access to all the parts of City. It is just 14.3 KM from the world famous MG Road Bangalore. The closest station from the Project is just 5.9 KM away. Want to reach the other parts of India through Airways? Then they have got you covered, the International Airport is just 13.2 KM away. The KR Puram Railway Station is Just nearby.
+
+
+<br/>
+ <br/>
+
+This property is also in the Vicinity to reach all the hospitals in case of emergencies. Some of the hospitals to reach include Columbia Asia hospital, VIMS Hospital, Janapriya Hospital, MGA Hospital and Many more.
+
+  <a href="http://concordeauriga.org.in/">Concorde Auriga</a> has a beautiful Gallery. The Floor Plan and Master Plan are designed in such a way that it will win the hearts of every Customer wishing to buy their dream homes. You can review the property by just visiting the site. You could leave all of your details here for free sit visit.
+
+
+
+
+
+
+
+
+</div>
     );
 };
 
